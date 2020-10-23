ea01f3dd41872d14e6d1882c74af5631b659e961
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2020-10-23 19:06:06 +0530

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# IMAGE DETECTION 
+
+# 
+# 
+# 
+# ## For further code, Please Mail me (vatshayan007@gmail.com) Now !!
+# 
+# ### This is Detailed Project, Mail me to get Full Project. 
+# #### Happy to help you 
+# 
+
+# In[2]:
+
+
+import cv2
+import numpy as np
+import pickle
+
+
+# 
